b0c6931297ad686f0cba2499b43c1c462ec86fb8
Tyler Long <tyler4long@gmail.com>
2022-11-29 14:36:59 -0800

    Add sample code 2

--- src/index2.ts
+++ src/index2.ts
@@ -1,9 +1,9 @@
-import RingCentral from 'ringcentral';
+import RingCentral from '@ringcentral/sdk';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
-  appKey: process.env.RINGCENTRAL_CLIENT_ID,
-  appSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
 });
 const platform = rc.platform();
 const main = async () => {
@@ -12,7 +12,8 @@ const main = async () => {
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD,
   });
-  const r = await platform.get('/account/~/extension/~');
-  console.log(r.json());
+  const r = await platform.get('/restapi/v1.0/account/~/extension/~');
+  const json = await r.json();
+  console.log(json);
 };
 main();
