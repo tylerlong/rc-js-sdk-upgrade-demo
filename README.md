85a8e87752ccd4b373d293e2b1d4477e290aa38c
Tyler Long <tyler4long@gmail.com>
2022-11-29 14:03:43 -0800

    First working demo

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # RingCentral JS SDK Upgrade Demo
 
 This is a demo project to tell you how to upgrade from 'ringcentral' to '@ringcentral/sdk'.
+
+
+## Setup
+
+Rename file `.env.sample` to `.env` and edit the file to specify credentials
+
+```
+yarn install
+```
+
+## Run
+
+### Run the demo for 'ringcentral'
+
+```
+yarn test
+```
+
+### Run the demo for '@ringcentral/sdk'
+
+```
+yarn test2
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import RingCentral from 'ringcentral';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  appKey: process.env.RINGCENTRAL_CLIENT_ID,
+  appSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+const platform = rc.platform();
+
+const main = async () => {
+  await platform.login({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD,
+  });
+  const r = await platform.get('/account/~/extension/~');
+  console.log(r.json());
+};
+main();
